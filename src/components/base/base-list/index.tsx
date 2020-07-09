754759283496470298178e832136352ab8c0dcb1
quyen.nguyen5 <quyen.nguyen@fiisoft.com>
2020-07-09 12:09:50 +0700

    remove class antd

--- src/components/base/base-list/index.tsx
+++ src/components/base/base-list/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { List, Avatar } from "antd";
 import avatar from "assets/images/avatar.svg";
+import "./style.scss";
 
 type dataItem = {
   id: string;
@@ -15,17 +16,29 @@ type BaseListProps = {
 
 export function BaseList({ data, extra }: BaseListProps) {
   return (
-    <List
-      dataSource={data}
-      renderItem={(item: any) => (
-        <List.Item key={item.id} extra={extra}>
-          <List.Item.Meta
-            avatar={<Avatar size="small" src={avatar} />}
-            title={<h3>{item.username}</h3>}
-            description={item.message}
-          />
-        </List.Item>
-      )}
-    />
+    <div className="item">
+      {data.map((item) => (
+        <div className="item-left">
+          <img className="avatar" src={avatar} />
+          <article className="content">
+            <h3>{item.username}</h3>
+            <p>{item.message}</p>
+          </article>
+        </div>
+      ))}
+      {extra}
+    </div>
+    // <List
+    //   dataSource={data}
+    //   renderItem={(item: any) => (
+    //     <List.Item key={item.id} extra={extra}>
+    //       <List.Item.Meta
+    //         avatar={<Avatar size="small" src={avatar} />}
+    //         title={<h3>{item.username}</h3>}
+    //         description={item.message}
+    //       />
+    //     </List.Item>
+    //   )}
+    // />
   );
 }
