e03d50be456b903494bca2d6a5eaf4fe5d0b5539
quyen.nguyen5 <quyen.nguyen@fiisoft.com>
2020-07-16 17:36:24 +0700

    feat: message item

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,15 @@
+import { type } from 'os';
+
 export type UserModel = {
     id: string;
     username: string;
 };
 export type MinimalItemData = { id: string };
+
+export type MessageType = {
+    userId: string;
+    username: string;
+    content: string;
+    read: boolean;
+    time: string;
+}
