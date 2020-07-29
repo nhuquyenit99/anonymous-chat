5ea7693a9d70b4b0cc548f37663ad1bbec534246
quyen.nguyen5 <quyen.nguyen@fiisoft.com>
2020-07-29 09:06:40 +0700

    chore: change gif + add title

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './style.scss';
-import gif from 'assets/images/Loading Screen 5_5 - Ducks.gif';
+import gif from 'assets/images/cool-loading-animated-gif-1.gif';
 export function Loading() {
     return (
         <div className='loading'>
+            <h2>Welcome to Anonymous Chat!</h2>
             <div className='gif'>
                 <img src={gif} alt="Loading..." />
             </div>
-            <p>Please wait for a minute.</p>
+            <p>Please wait for a minute...</p>
         </div>
     );
 }
